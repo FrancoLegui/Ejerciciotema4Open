cdffb19dc2594bb6f6e630abcef5536d5960ad51
FrancoLegui <franbrass2@gmail.com>
2023-05-20 00:05:59 -0300

    segundo ejercicio Open, Tema 4

---
+++ Totalidad de Ejercicios/Ejercicio Tema 4 OpenBootCamp/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = 0;
+
+        if (numeroIf > 0) {
+            System.out.println("El número es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("El número es negativo");
+        } else {
+            System.out.println("El número es igual a cero");
+        }
+
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            numeroWhile++;
+            System.out.println("Valor de numeroWhile: " + numeroWhile);
+        }
+
+        int numeroDoWhile = 0;
+        do {
+            numeroDoWhile++;
+            System.out.println("Valor de numeroDoWhile: " + numeroDoWhile);
+        } while (numeroDoWhile < 3);
+
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println("Valor de numeroFor: " + numeroFor);
+        }
+
+        String estacion = "hongo";
+        switch (estacion) {
+            case "primavera":
+                System.out.println("Estamos en primavera");
+                break;
+            case "verano":
+                System.out.println("Estamos en verano");
+                break;
+            case "otoño":
+                System.out.println("Estamos en otoño");
+                break;
+            case "invierno":
+                System.out.println("Estamos en invierno");
+                break;
+            default:
+                System.out.println("No es una estación");
+                break;
+        }
+    }
+}
